e725ae52a5922d68a53e5a4ad429f73d3b72ac6a
M <jenky2010@gmail.com>
2026-02-22 23:24:08 +0200

    feat: created models for db

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    project = models.ForeignKey(
+        Project, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    title = models.CharField(max_length=160)
+    description = models.TextField(blank=True)
+
+    due_date = models.DateField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["is_done", "-created_at"]
+
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Project, Task
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home.html')
-
-
-# Create your views here.
+        print(request.user.id)
+        print(request.__dict__)
+        projects = Project.objects.filter(user=request.user.id)
+        tasks = Task.objects.filter(user=request.user.id)
+        print(tasks)
+        return render(request, "home.html", {"projects": projects, "tasks": tasks})
