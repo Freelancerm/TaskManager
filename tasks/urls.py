1c6b4cad3a1a018616625676e7463c4b6bd317dc
M <jenky2010@gmail.com>
2026-02-23 11:32:23 +0200

    feat: provided check box for task is done. with partial htmx elements

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, TaskToggleDoneView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("tasks/<int:pk>/toggle/", TaskToggleDoneView.as_view(), name="task-toggle"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Project, Task
 
 
@@ -8,3 +10,11 @@ class HomeView(View):
         projects = Project.objects.filter(user=request.user)
         tasks = Task.objects.filter(user=request.user)
         return render(request, "home.html", {"projects": projects, "tasks": tasks})
+
+
+class TaskToggleDoneView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk, user=request.user)
+        task.is_done = not task.is_done
+        task.save(update_fields=["is_done"])
+        return render(request, "partials/task_item.html", {"task": task})
