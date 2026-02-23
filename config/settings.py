dd14e402921ee8c926c9822956542f422643c858
M <jenky2010@gmail.com>
2026-02-23 14:36:32 +0200

    feat: provided login/signup pages with custom stylization

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,6 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
+
+ACCOUNT_EMAIL_VERIFICATION = "none"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
